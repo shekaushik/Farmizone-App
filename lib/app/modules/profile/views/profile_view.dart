b99c23f7b619d41f22bb0cf03571414dab69e6b9
Akanksha Swati <akankshaswati333@gmail.com>
2024-04-12 19:46:29 +0530

    profile page modified

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../components/no_data.dart';
+import '../../../components/custom_button.dart';
 import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
@@ -14,7 +16,125 @@ class ProfileView extends GetView<ProfileController> {
         title: Text('Profile', style: context.theme.textTheme.headline3),
         centerTitle: true,
       ),
-      body: const NoData(text: 'This is Profile Screen'),
+      body: Container(color: Colors.white54,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircleAvatar(
+                  maxRadius: 65,
+                  backgroundImage: AssetImage("assets/avatar.png"),
+                ),
+              ],
+            ),
+            24.verticalSpace,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+
+                Text(
+                  "ABC XYZ",
+                  style:
+                  TextStyle(fontWeight: FontWeight.w900, fontSize: 26),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              child: Expanded(
+                  child: ListView(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 70.w),
+                        child: CustomButton(
+                          text: 'Privacy',
+                          onPressed: () {},
+                          fontSize: 16.sp,
+                          radius: 50.r,
+                          verticalPadding: 16.h,
+                          hasShadow: false,
+                        ).animate().fade().slideY(
+                          duration: 300.ms,
+                          begin: 1,
+                          curve: Curves.easeInSine,
+                        ),
+                      ),
+                      24.verticalSpace,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 70.w),
+                        child: CustomButton(
+                          text: 'Purchase History',
+                          onPressed: () {},
+                          fontSize: 16.sp,
+                          radius: 50.r,
+                          verticalPadding: 16.h,
+                          hasShadow: false,
+                        ).animate().fade().slideY(
+                          duration: 300.ms,
+                          begin: 1,
+                          curve: Curves.easeInSine,
+                        ),
+                      ),
+                      24.verticalSpace,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 70.w),
+                        child: CustomButton(
+                          text: 'Help and Support',
+                          onPressed: () {},
+                          fontSize: 16.sp,
+                          radius: 50.r,
+                          verticalPadding: 16.h,
+                          hasShadow: false,
+                        ).animate().fade().slideY(
+                          duration: 300.ms,
+                          begin: 1,
+                          curve: Curves.easeInSine,
+                        ),
+                      ),
+                      24.verticalSpace,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 70.w),
+                        child: CustomButton(
+                          text: 'Settings',
+                          onPressed: () {},
+                          fontSize: 16.sp,
+                          radius: 50.r,
+                          verticalPadding: 16.h,
+                          hasShadow: false,
+                        ).animate().fade().slideY(
+                          duration: 300.ms,
+                          begin: 1,
+                          curve: Curves.easeInSine,
+                        ),
+                      ),
+                      24.verticalSpace,
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 70.w),
+                        child: CustomButton(
+                          text: 'Logout',
+                          onPressed: () {},
+                          fontSize: 16.sp,
+                          radius: 50.r,
+                          verticalPadding: 16.h,
+                          hasShadow: false,
+                        ).animate().fade().slideY(
+                          duration: 300.ms,
+                          begin: 1,
+                          curve: Curves.easeInSine,
+                        ),
+                      ),
+                    ],
+                  )),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
